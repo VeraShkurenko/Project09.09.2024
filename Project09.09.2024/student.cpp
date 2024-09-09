7b3761199b0a1cac4544c11fe3ee38a0cfdba1f4
Skur_pn81 <Skur_pn81@28-1>
2024-09-09 09:58:20 +0300

    динамический масив

--- Project09.09.2024/student.cpp
+++ Project09.09.2024/student.cpp
@@ -39,20 +39,47 @@ public:
         Sleep(1000);
 
     }
+    void Init()
+    {
+        char buff[20];
+        cout << "Enter name -> ";
+        cin >> buff; // Oleg
 
+        name = new char[strlen(buff) + 1];
+        strcpy_s(name, strlen(buff) + 1, buff);
+        cout << "Enter age-> ";
+        cin >> age;
+    }
 
 };
 
 int main()
 {
-    Student obj1("Oleg", 21);
-    obj1.Output();
+    //Student obj1("Oleg", 21);
+    //obj1.Output();
 
-    /*Student mas[2]{ {"Alena",20 }, {"Maksim", 22} };
+   /* Student mas[2]{ {"Alena",20 }, {"Maksim", 22} };
     for (int i = 0; i < 2; i++)
     {
       mas[i].Output();
       }*/
+
+    int count;
+    cout << "Enter count -> ";
+    cin >> count; // 3
+
+    Student* mas = new Student[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        mas[i].Init();
+    }
+    for (int i = 0; i < count; i++)
+    {
+        mas[i].Output();
+    }
+
+    delete[] mas;
 
 
 
